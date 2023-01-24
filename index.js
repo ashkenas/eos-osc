599631d4ca42e40fc56cc134a50f49a7557040c0
Jacob Ashkenas <jacobashkenas@gmail.com>
2023-01-23 20:19:08 -0500

    Raw OSC console

---
+++ index.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const os = require('os');
+const express = require('express');
+const OSC = require('osc-js');
+
+let addr;
+for (const interf of Object.values(os.networkInterfaces())) {
+    if (interf[0].internal) continue;
+    for (const network of interf) {
+        if (network.family !== 'IPv4' && network.family !== 4) continue;
+        addr = network.address;
+        break;
+    }
+    if (addr) break;
+}
+if (!addr) {
+    console.error('No network address found.');
+    process.exit(1);
+}
+
+const osc = new OSC({
+    plugin: new OSC.BridgePlugin({
+        receiver: 'udp',
+        udpServer: {
+            host: addr,
+            port: 4704
+        },
+        udpClient: {
+            host: addr,
+            port: 4703
+        },
+        wsServer: {
+            host: 'localhost',
+            port: 8080
+        }
+    })
+})
+
+osc.on('*', message => console.log(message));
+
+osc.on('open', () => {
+    console.log(`OSC server running on ${addr}:4704`)
+    osc.send(new OSC.Message('/eos/subscribe',1));
+});
+
+osc.on('error', (e) => console.error(e));
+
+osc.open();
+
+const app = express();
+
+const static = express.static(path.join(__dirname, '/public'));
+app.use(static);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+});
+
+app.get('/osc.js', (req, res) => {
+    res.sendFile(path.join(__dirname, 'node_modules/osc-js/lib/osc.min.js'))
+});
+
+app.listen(80, () => console.log('Server online at http://localhost/.'));
