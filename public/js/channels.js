9a35d0982dbeadee56eb95f6594591c9d910db92
Jacob Ashkenas <jacobashkenas@gmail.com>
2023-01-24 16:48:19 -0500

    Display for channel values

--- public/js/channels.js
+++ public/js/channels.js
@@ -1,4 +1,7 @@
 const channels = {};
+const channelElements = [];
+const channelsContainer = document.getElementById('channels');
+
 const channelPattern = /patch\/([0-9]*)\//i;
 const osc = new OSC({
     plugin: new OSC.WebsocketClientPlugin({
@@ -24,14 +27,57 @@ osc.on('/eos/out/get/patch/count', message => {
 osc.on('/eos/out/get/patch/*/list/*', message => {
     if (message.args[4].toLowerCase().includes('non_dim')) return;
     const [, channel] = channelPattern.exec(message.address);
-    channels[channel] = message.args[7];
+    updateChannel(channel, message.args[7]);
 });
 
 osc.on('/eos/out/active/wheel/*', message => {
     if (!message.args[0].toLowerCase().startsWith('intens')) return;
     channels[message.args[1]] = message.args[2];
+    updateChannel(message.args[1], message.args[2]);
 });
 
 osc.on('error', (err) => console.error(err));
 
 osc.open();
+
+const insertChannel = (chan, chanVal) => {
+    const name = document.createElement('p');
+    name.innerText = chan;
+    const val = document.createElement('p');
+    val.innerText = chanVal;
+    const el = document.createElement('div');
+    el.setAttribute('data-chan', chan);
+    el.append(name, val);
+
+    if (!channelElements.length) {
+        channelElements.unshift(el)
+        channelsContainer.prepend(el);
+        return;
+    }
+
+    for (let i = 0; i < channelElements.length; i++) {
+        if (chan > +channelElements[i].getAttribute('data-chan'))
+            continue;
+
+        channelElements[i].before(el);
+        channelElements.splice(i, 0, el);
+        return;
+    }
+
+    channelElements.push(el)
+    channelsContainer.append(el);
+};
+
+const updateChannel = (chan, val) => {
+    if (chan in channels) {
+        for (let i = 0; i < channelElements.length; i++) {
+            if (chan !== +channelElements[i].getAttribute('data-chan')) continue;
+    
+            channelElements[i].children[1].innerText = val;
+            return;
+        }
+    } else {
+        insertChannel(chan, val);
+    }
+    channels[chan] = val;
+};
